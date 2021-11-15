4f6b9ca54d2b147ed4ef26cd80c099c5f93fea33
Olakunle Neye <olakunle.neye@thoughtworks.com>
2021-11-15 15:34:24 -0500

    [on] created account route to handle account requests

--- server.js
+++ server.js
@@ -1,50 +1,12 @@
 "use strict";
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const mongoose = require("mongoose");
-
-const bodyParser = require('body-parser');
 require("dotenv").config();
 
-var routes = require('./api/routes/bankRoutes'); //importing route
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
+const accountRoutes = require('./api/routes/account.routes');
 const Account = require("./api/models/account");
-
-// TODO - Remove the following
-// var Schema = mongoose.Schema;
-// const Account = mongoose.model('Account', accountSchema);
-// dummyAccount.save().then(() => console.log("new account added"));
-// mongoose.Promise = global.Promise;
-// // mongoose
-// //   .connect("mongodb://localhost:27017/Bankdb")
-// //   .catch((error) => console.error(error)); 
-//   try {
-//      mongoose.connect("mongodb://localhost:8080/Bankdb");
-//   } catch (error) {
-//     handleError(error);
-//   }
-// async function listDatabases(client) {
-//   const databasesList = await client.db().admin().listDatabases();
-
-//   console.log("Databases: ");
-//   databasesList.databases.forEach((db) => console.log(` -${db.name}`));
-// }
-// async function main() {
-//   const url =
-//     "mongodb+srv://Khifayat:BforBolu99@cluster0.2az67.mongodb.net/BudgetSmartBank?retryWrites=true&w=majority";
-//   const client = new MongoClient(url);
-
-//   try {
-//     await client.connect();
-//     await listDatabases(client);
-//   } catch (e) {
-//     console.error(e);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// main().catch(console.error);
-
 
 // Constants
 const PORT = 8080;
@@ -64,35 +26,14 @@ const dummyAccount = new Account({
   account_number: '123455',
   created_date: Date.now,
   current_balance: 50.50
-
 });
 
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-routes(app); //register the route
 
+app.use("/account", accountRoutes);
 
-// Endpoints
-// GET HOME
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-// app.post('/create-account', () => {
-// });
-
-
-// create an account POST
-// get information GET
-app.get('/accounts', (req, res) => {
-  Account.find({}, (found, err) => {
-    if (!err) {
-      res.send(found);
-    }
-    console.log(err);
-    res.send("error occured!")
-  })
-})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
