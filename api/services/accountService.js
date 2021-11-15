25e83ab1b32e36fe384f8ec743f52e5b0f67ba15
Olakunle Neye <olakunle.neye@thoughtworks.com>
2021-11-15 15:35:01 -0500

    [on] created account services to handle account requests ans response

---
+++ api/services/accountService.js
@@ -0,0 +1,25 @@
+const Account = require("../models/account");
+
+module.exports = class AccountService {
+    static async getAllAccounts() {
+        try {
+            const allAccounts = await Account.find();
+            return allAccounts;
+        } catch (error) {
+            console.error(`could not fetch the accounts ${error}`);
+        }
+    }
+    static async createAccount(data) {
+        try {
+            const account = {
+                account_number: data.account_number,
+                current_balance: data.current_balance,
+            };
+            const response = await new Account(account).save();
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+}
